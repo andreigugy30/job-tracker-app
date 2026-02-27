a3cd4ded4abd4536cf4748e526ccc2ab206ef77b
Andrei <andrei.gugy@gmail.com>
2026-02-27 17:29:00 +0200

    finish auth with sign in and sign out

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -11,9 +11,39 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "@/lib/auth/auth-client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function SignIn() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	const router = useRouter();
+
+	async function handleFormSubmit(event: FormEvent) {
+		event.preventDefault();
+		setError("");
+		setLoading(true);
+		try {
+			const result = await signIn.email({
+				email,
+				password,
+			});
+			if (result.error) {
+				setError(result.error.message ?? "Failed to sign in");
+			} else {
+				router.push("/dashboard");
+			}
+		} catch (e) {
+			setError("An unexpected error occured");
+		} finally {
+			setLoading(false);
+		}
+	}
 	return (
 		<div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p=4">
 			<Card className="w-full max-w-md border-gray-200 shadow-lg">
@@ -25,7 +55,12 @@ export default function SignIn() {
 						Enter your credentials to access the account
 					</CardDescription>
 				</CardHeader>
-				<form className="space-y-4">
+				<form className="space-y-4" onSubmit={handleFormSubmit}>
+					{error && (
+						<div className="rounded-md bg-destructive/15 p-3 tex-sm text-destructive">
+							{error}
+						</div>
+					)}
 					<CardContent className="space-y-4">
 						<div className="space-y-2">
 							<Label htmlFor="email" className="text-gray-700">
@@ -37,6 +72,8 @@ export default function SignIn() {
 								placeholder="john.doe@gmail.com"
 								required
 								className="border-gray-300 focus:border-primary focus:ring-primary"
+								value={email}
+								onChange={(ev) => setEmail(ev.target.value)}
 							></Input>
 						</div>
 						<div className="space-y-2">
@@ -50,12 +87,18 @@ export default function SignIn() {
 								required
 								minLength={8}
 								className="border-gray-300 focus:border-primary focus:ring-primary"
+								value={password}
+								onChange={(ev) => setPassword(ev.target.value)}
 							></Input>
 						</div>
 					</CardContent>
 					<CardFooter className="flex flex-col space-y-4">
-						<Button type="submit" className="text-center text-sm text-gray-600">
-							Sign in
+						<Button
+							type="submit"
+							className="text-center text-sm text-gray-600"
+							disabled={loading}
+						>
+							{loading ? "Signing in..." : "Sign in"}
 						</Button>
 						<p>
 							Don't have an account?{" "}
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -8,3 +10,22 @@ export const auth = betterAuth({
 	database: mongodbAdapter(db, { client }),
 	emailAndPassword: { enabled: true },
 });
+
+//Use auth to detect if there is a current user sign in - for server components
+export async function getSession() {
+	const result = auth.api.getSession({
+		headers: await headers(),
+	});
+	return result;
+}
+
+//Sign Out - in server components
+
+export async function signOut() {
+	const result = auth.api.signOut({
+		headers: await headers(),
+	});
+	if ((await result).success) {
+		redirect("/sign-in");
+	}
+}
